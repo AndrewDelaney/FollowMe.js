7319f76a050600fc7f33661b680aa935425a0bec
Andrew Delaney <30876243+AndrewDelaney@users.noreply.github.com>
2021-04-22 16:00:12 +0200

    Update FollowMe.js description

--- FollowMe.js
+++ FollowMe.js
@@ -5,28 +5,18 @@
 
 /*
 Give any element that you wish to add the Follow Mouse effect to
-a class of "Sheep"...because, you know...sheep follow things.
+a class of "Sheep".
 
 After you have given the elements their "Sheep" classes, add a
-"data-resistance" attribute to the element.
+"data-resistance" attribute to the html element.
 
-"data-resistance" is a special attribute that I have defined, so
-don't freak out if you've never seen it before and don't know what
-it does. Basically, it adds resistance to the object that you add 
-the attribute to, so that the object follows your mouse at a slower 
-speed (or moves away from your mouse).
+"data-resistance" adds resistance to the object so that the object
+follows your mouse at a slower speed (or moves away from your
+mouse). The value you define is converted into a fraction and
+therefore cannot be 0
 
-The way you write the attribute into your code, if you can't work 
-that out, is exactly as I have quoted it previously. Simply type
---> data-resistance = "" <-- just as you would any other html 
-attribute, such as class = "", id = "", width = "", etc. In this
-case, the input requires an integer value (positive or negative),
-but cannot be zero due to the fact that the maths divides by the
-number you input. If you input zero a black hole will form...then 
-we're all screwed.
-
-So basically, if you give it a value of 1, the objects will follow
-your mouse at it's exact speed. If you give it a 2, it will half
+If you give it a value of 1, the objects will follow
+your mouse at at a 1:1 ration. If you give it a 2, it will half
 that speed, and so on. And if you want it to move opposite to your
 mouse, you give it a negative value. If you're looking for a very
 subtle shift of your elements, I'd recommend giving it a value of 
@@ -34,8 +24,6 @@ around 200.
 
 Hopefully that gave you an idea of the range of numbers you'll
 want to use.
-
-Have fun using this plugin! It isn't a difficult one to understand.
 */
 
 function followMe(){
@@ -61,7 +49,7 @@ function followMe(){
 	var xPercent = (mouseX/windowX*100);
 	var yPercent = (mouseY/windowY*100);
 	
-	//Just storing the value that is half the screen height and width
+	//Storing the value that is half the screen height and width
 	var halfScreenx = windowX/2;
 	var halfScreeny = windowY/2;
 	
@@ -118,5 +106,5 @@ function followMe(){
 	}
 }
 
-//Make sure it runs all this code every time the mouse moves...its tracking you ;)
+//Trigger function on mousemove
 document.onmousemove = followMe;
